714926793c2d6e9c0a4763231db6b4fa7f272ec6
Soufian BOUTAIB <soufian.boutaib@fwa.eu>
2023-07-23 14:18:46 +0200

    Add project info model

--- energy-eligibility/src/app/modules/project-info/project-info.component.ts
+++ energy-eligibility/src/app/modules/project-info/project-info.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { ManageFormService } from '../../services/manage-form-service';
+import { OwnershipEnum } from 'src/app/models/project-info-model';
 
 @Component({
   selector: 'app-project-info',
   templateUrl: './project-info.component.html',
-  styleUrls: ['./project-info.component.css']
+  styleUrls: ['./project-info.component.css'],
 })
-export class ProjectInfoComponent {
+export class ProjectInfoComponent implements OnInit {
+  form: FormGroup;
+  ownershipChoices = Object.values(OwnershipEnum);
+  formattedIncome: string = '';
+  submitted = false;
 
+  constructor(
+    private router: Router,
+    private formService: ManageFormService,
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      ownership: ['', Validators.required],
+      numberOfPeople: ['', [Validators.required, Validators.min(1)]],
+      householdIncome: [
+        '',
+        [Validators.required, Validators.min(10000), Validators.max(100000)],
+      ],
+      area: ['', [Validators.required, Validators.min(1)]],
+    });
+  }
+
+  ngOnInit(): void {
+    this.formService.currentProjectInfoForm.subscribe((form) =>
+      this.form.patchValue(form)
+    );
+  }
+
+  next(): void {
+    this.submitted = true;
+
+    if (this.form.valid) {
+      this.formService.changeProjectInfoForm({
+        ...this.form.value,
+        area: +this.form.value.area,
+      });
+      this.router.navigate(['/summary']);
+    }
+  }
 }
